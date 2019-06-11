44b8a3b38d2af0c2c5c0a09473aac6484093e368
rclarey <r.e.clarey@gmail.com>
2019-06-11 17:00:02 -0400

    got css working and /signin working

--- web_service/public_web/public_web_express_router.js
+++ web_service/public_web/public_web_express_router.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const expressUtils = require('shared/util/expressUtils');
+const express = require("express");
+const expressUtils = require("shared/util/expressUtils");
 
 const publicWebExpressRouter = (() => {
-    const router = expressUtils.createRouter();
+  const router = expressUtils.createRouter();
 
-    router.use(express.static('web_service/static'));
+  router.use(express.static("web_service/static"));
+  router.get("/*", (_, res) =>
+    res.sendFile("web_service/static/index.html", { root: "." }),
+  );
 
-    return router;
+  return router;
 })();
 
 module.exports = publicWebExpressRouter;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
-const path = require('path');
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './client/index.js',
-    output: {
-        path: path.join(__dirname, 'web_service/static'),
-        filename: 'client_bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$|/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    // {loader: 'style-loader'},
-                    {loader: 'css-loader'},
-                ],
-            },
-        ],
-    },
+  mode: "development",
+  entry: "./client/index.js",
+  output: {
+    path: path.join(__dirname, "web_service/static"),
+    filename: "client_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$|/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
 };
