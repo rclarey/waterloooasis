0f0567f8376d8666ee4a1c91e8fdc8877348b1ce
Raymond Lam <raymondlam197@gmail.com>
2019-06-12 15:17:52 -0400

    update

--- web_service/private_api/private_api_express_router.js
+++ web_service/private_api/private_api_express_router.js
@@ -1,18 +1,7 @@
 const expressUtils = require('shared/util/expressUtils');
-const mysql = require('mysql')
-const pool = require('shared/util/db.js');
 
 const privateApiExpressRouter = (() => {
   const router = expressUtils.createRouter();
-
-  router.get('/api/users', (_, res) => {
-    pool.query('SELECT * FROM waterloooasis', function(err, rows, fields) {
-      if (err) throw err;
-      /* Manage your results here */
-
-    });
-
-  });
 
   return router;
 })();
--- web_service/public_web/public_web_express_router.js
+++ web_service/public_web/public_web_express_router.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const expressUtils = require("shared/util/expressUtils");
+const pool = require('shared/util/db.js');
+const bodyParser = require('body-parser');
 
 const publicWebExpressRouter = (() => {
   const router = expressUtils.createRouter();
 
   router.use(express.static("web_service/static"));
+  router.use(bodyParser.urlencoded());
+  router.use(bodyParser.json());
+
   router.get("/*", (_, res) =>
     res.sendFile("web_service/static/index.html", { root: "." }),
   );
+
+  /**
+  FOR NOW PUT THIS HERE TO TEST EXTERNALLY
+  */
+  router.get('/api/users', (_, res) => {
+    pool.query('SELECT * FROM waterloo_oasis_dev.user', function(err, rows, fields) {
+      if (err) throw err;
+      res.send(rows);
+    });
+  });
+
+  /**
+  FOR NOW: Pass in username, rest will be set values
+  */
+  router.post('/api/users', (req, res) => {
+    console.log(req.body);
+    pool.query('SELECT * FROM waterloo_oasis_dev.user', function(err, rows, fields) {
+      if (err) throw err;
+      /* Manage your results here */
+
+    });
+
+  });
 
   return router;
 })();
