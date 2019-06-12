9b5cd9badf1fa3f919055033a82cefd43340bfd7
Raymond Lam <raymondlam197@gmail.com>
2019-06-12 14:12:31 -0400

    update

--- web_service/private_api/private_api_express_router.js
+++ web_service/private_api/private_api_express_router.js
@@ -1,12 +1,20 @@
 const expressUtils = require('shared/util/expressUtils');
 const mysql = require('mysql')
+const pool = require('shared/util/db.js');
 
 const privateApiExpressRouter = (() => {
-    const router = expressUtils.createRouter();
+  const router = expressUtils.createRouter();
 
-    router.get()
+  router.get('/api/users', (_, res) => {
+    pool.query('SELECT * FROM waterloooasis', function(err, rows, fields) {
+      if (err) throw err;
+      /* Manage your results here */
 
-    return router;
+    });
+
+  });
+
+  return router;
 })();
 
 module.exports = privateApiExpressRouter;
