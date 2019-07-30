6da3bb600d8a9064d88dc558922ef313d07c72e9
Dane Nespoli <danenespoli@gmail.com>
2019-07-30 19:59:43 -0400

    wip

--- client/oasisui/OLink.jsx
+++ client/oasisui/OLink.jsx
@@ -1,18 +1,26 @@
 import React, { memo } from 'react';
+import { Link } from '@reach/router';
 import PropTypes from 'prop-types';
 import * as classNames from 'classnames';
 
 import 'oasisui/OLink.css';
 
-function OLink({ text, light, large, link }) {
+function OLink({ text, light, large, link, isRouterLink }) {
   const classes = classNames('olink__container', {
     olink__light: light,
     olink__dark: !light,
     olink__large: large,
     olink__small: !large,
   });
+
+  if (isRouterLink) {
+    <Link className={classes} to={link}>
+      {text}
+    </Link>
+  }
+
   return (
-    <a href={link} className={classes}>
+    <a className={classes} href={link}>
       {text}
     </a>
   );
@@ -23,6 +31,7 @@ OLink.propTypes = {
   light: PropTypes.bool,
   large: PropTypes.bool,
   link: PropTypes.string.isRequired,
+  isRouterLink: PropTypes.bool,
 };
 
 export default memo(OLink);
