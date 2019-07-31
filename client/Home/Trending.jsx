01bb1bc52919131c357222598d9008bfde64289f
Dane Nespoli <danenespoli@gmail.com>
2019-07-31 01:20:38 -0400

    prettier

--- client/Home/Trending.jsx
+++ client/Home/Trending.jsx
@@ -4,7 +4,7 @@ import useFetch from 'fetch-suspense';
 
 import JobTile from 'shared/JobTile.jsx';
 
-import 'Home/Trending.css'
+import 'Home/Trending.css';
 
 function Trending() {
   const trending = useFetch('http://localhost:3000/api/trending');
