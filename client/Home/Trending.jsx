d7c29b13028ecaca246171b555699a70669b1324
Dane Nespoli <danenespoli@gmail.com>
2019-07-31 03:30:50 -0400

    wip

--- client/Home/Trending.jsx
+++ client/Home/Trending.jsx
@@ -7,7 +7,7 @@ import JobTile from 'shared/JobTile.jsx';
 import 'Home/Trending.css';
 
 function Trending() {
-  const trending = useFetch('http://localhost:3000/api/trending');
+  const trending = useFetch('api/trending');
 
   return (
     <>
