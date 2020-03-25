b6ba8c2dacf0c379dfe79888d507dca49794e163
Raymond Lam <r32lam@edu.uwaterloo.ca>
2020-03-25 02:39:34 -0400

    Add UI to indicate lack of search results

--- client/Home/SearchResults.jsx
+++ client/Home/SearchResults.jsx
@@ -7,32 +7,40 @@ import queryString from 'query-string';
 import CompanyTile from 'shared/CompanyTile.jsx';
 import JobTile from 'shared/JobTile.jsx';
 
+import 'Home/SearchResults.css';
+
 function SearchResults() {
   const queryParams = queryString.parse(location.search);
   const results = useFetch(`/api/search/${queryParams.q}`);
+  console.log(results);
 
   return (
     <>
-      {results.companies.map(company => (
-        <Link
-          to={`/company/${company.id}`}
-          key={company.id}
-        >
-          <CompanyTile company={company} />
-        </Link>
-      ))}
-      {/* NO MORE JOB POSTINGS */}
-      {/* {results.jobs.map(job => (
-        <Link
-          to={`/jobs/${job.company.shortName}/${job.shortCode}`}
-          key={job.id}
-        >
-          <JobTile job={job} />
-        </Link>
-      ))} */}
-      <div className="trending__overlay" />
+      {results.companies.length > 0 ? (
+        results.companies.map(company => (
+          <Link
+            to={`/company/${company.id}`}
+            key={company.id}
+          >
+            <CompanyTile company={company} />
+          </Link>
+        ))
+      ) : (
+        <MemoedNoSearchResults />
+      )}
+      <div className="search__overlay" />
     </>
   );
 }
+
+function NoSearchResults() {
+  return (
+    <div className="search__nonecompanies">
+      <p className="search__noneheader">No Results!</p>
+      <p>Please try again with other search terms.</p>
+    </div>
+  );
+}
+const MemoedNoSearchResults = memo(NoSearchResults);
 
 export default memo(SearchResults);
--- web_service/private_api/search_routes.js
+++ web_service/private_api/search_routes.js
@@ -20,29 +20,6 @@ const searchRouter = (() => {
       GROUP BY c.id, c.name
     `;
 
-    const selectJobs = `
-      SELECT job.*, company.name, company.short_name FROM job
-      INNER JOIN company ON job.company_id = company.id
-      WHERE job.title LIKE ? OR company.name LIKE ?`;
-    const selectRating = `
-      SELECT job.id, COUNT(rating) as num_reviewers, SUM(rating) as total_rating FROM job
-      LEFT JOIN ratings ON job.id = ratings.job_id
-      INNER JOIN company ON job.company_id = company.id
-      WHERE job.title LIKE ? OR company.name LIKE ?
-      GROUP BY job.id ORDER BY job.id`;
-    const selectComment = `
-      SELECT job.id, COUNT(comment.id) as num_comments FROM job
-      LEFT JOIN comment ON job.id = comment.job_id
-      INNER JOIN company ON job.company_id = company.id
-      WHERE job.title LIKE ? OR company.name LIKE ?
-      GROUP BY job.id ORDER BY job.id`;
-    const selectFollows = `
-      SELECT job.id, COUNT(following.id) as num_follows FROM job
-      LEFT JOIN following ON job.id = following.job_id
-      INNER JOIN company ON job.company_id = company.id
-      WHERE job.title LIKE ? OR company.name LIKE ?
-      GROUP BY job.id ORDER BY job.id`;
-
     try {
       if (!req.params.query) {
         throw { badRequest: true };
@@ -50,51 +27,8 @@ const searchRouter = (() => {
 
       const companyOverview = await query(selectCompanyByQuery, [`%${req.params.query}%`]);
 
-      const jobs = await query(selectJobs, [`%${req.params.query}%`, `%${req.params.query}%`]);
-      const ratings = await query(selectRating, [`%${req.params.query}%`, `%${req.params.query}%`]);
-      const comments = await query(selectComment, [`%${req.params.query}%`, `%${req.params.query}%`]);
-      const follows = await query(selectFollows, [`%${req.params.query}%`, `%${req.params.query}%`]);
-
-      const jobsJson = jobs.map((job, i) => {
-        const { num_reviewers, total_rating } = ratings[i];
-        const { num_comments } = comments[i];
-        const { num_follows } = follows[i];
-
-        const totalRating =
-          num_reviewers === 0
-            ? 0
-            : Number((total_rating / num_reviewers).toPrecision(3));
-
-        const statusString = `${job.status}${
-          job.status_round > 1 ? ` (round ${job.status_round})` : ''
-        }`;
-
-        return {
-          totalRating,
-          company: {
-            id: job.company_id,
-            name: job.name,
-            shortName: job.short_name,
-          },
-          id: job.id,
-          shortCode: job.short_code,
-          title: job.title,
-          status: {
-            string: statusString,
-            round: job.status_round,
-          },
-          statusStage: job.status_stage,
-          location: job.location,
-          description: job.description,
-          numReviewers: num_reviewers,
-          numComments: num_comments,
-          numFollows: num_follows,
-        };
-      });
-
       res.status(200).json({
-        companies: companyOverview,
-        jobs: jobsJson,
+        companies: companyOverview
       });
     } catch(e) {
       res.status(500).json({ reason: 'Something went wrong' });
